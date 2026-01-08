3bffd3c458f52e2aa3dc49b8c962a8021e37dc5b
CarvalhoNathan <fariasnathan75@gmail.com>
2026-01-07 22:51:54 -0300

    Update Program.cs: set Pessoa properties and demonstrate console output

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// O namespace é o caminho único da classe, porém caso eu queira usar a mesma classe, eu preciso alterar apenas o nome do namespace e assim consigo continuar usando a classe.
+
+using tivit_net_github_copilot.Models;
+
+Pessoa p = new Pessoa();
+
+p.Nome = "Nathan";
+p.Idade = 23;
+p.Apresentar();
+
+// Pessoa pessoaFisica = new Pessoa();
+// Exempelo de camelCase em variável
+// Único caracter especial permitido em variável é o _ (underline)
